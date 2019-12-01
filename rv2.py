b4def8c0277850614838ea249d20a82d007f92ca
digital-knife <jermainerakoczy1@gmail.com>
2019-12-01 14:23:33 -0600

    first wave of snake charming

---
+++ rv2.py
@@ -0,0 +1,28 @@
+import argparse
+
+#build a parser
+parser = argparse.ArgumentParser(description='Read a file in reverse')
+parser.add_argument('filename', help='the file to read')
+parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+args = parser.parse_args()
+
+
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+    
+    for line in lines:
+        print(line.strip()[::-1])
+
+
+#print(args)
+
+#parse the arguments
+
+#carry out the script
+
+# read the file, reverse contents and print
